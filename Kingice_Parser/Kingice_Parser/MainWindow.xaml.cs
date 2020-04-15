a58665f37036912fa3937d2c8ee751fb10d40b5f
Artur Khayrullin <kehel@yandex.ru>
2020-04-15 17:53:56 +0300

    Add class data wrapper

--- Kingice_Parser/Kingice_Parser/MainWindow.xaml.cs
+++ Kingice_Parser/Kingice_Parser/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Collections.Generic;
+using System.Linq;
+using HtmlAgilityPack;
 using System.IO;
 
 namespace Kingice_Parser {
@@ -12,52 +14,49 @@ namespace Kingice_Parser {
         }
 
         private void ButtonParse_Click(object sender, RoutedEventArgs e) {
+
+            string hrefRoot = "https://www.kingice.com";
+
             for (int fileNum = 1; fileNum < 22; fileNum++) {
                 string filePath = string.Format("C:\\Users\\Artur\\Downloads\\Html\\{0}.html", fileNum);
-                var all = File.ReadAllLines(filePath);
 
-                List<string> result = new List<string>();
-
-                string imageSrc = "";
-                string productHref = "";
-                string productTitle = "";
-                string price = "";
-                List<string> Types = new List<string>();
+                List<Product> result = new List<Product>();
+                Product current = null;                          
                 int num = 0;
-                bool isFirst = true;
 
+                var all = File.ReadAllLines(filePath);
                 foreach (var item in all) {
                     if (item.Contains("<div class=\"box product\">")) {
-                        if (isFirst) {
-                            isFirst = false;
-                        }
-                        else {
-                            result.Add(string.Format("{0}@{1}@{2}@{3}@{4}@{5}", num, imageSrc.Trim(), productHref.Trim(), productTitle.Trim(), price.Trim(), string.Join(", ", Types).Trim()));
+                        if (current != null) {
+                            result.Add(current);
                             num++;
-                            Types.Clear();
                         }
+                        current = new Product();
                     }
 
-                    if (item.Contains("<img class=\"product_card__image\" src=")) {
-                        var tmp = item.Replace("<img class=\"product_card__image\" src=\"", "");
-                        imageSrc = tmp.Substring(0, tmp.IndexOf('"'));
-                    }
-                    if (item.Contains("<a href=\"/collections/all/") && item.Contains("class=\"title\">")) {
-                        productTitle = item.Substring(item.IndexOf('>')).Replace("</a>", "");
-                    }
-                    if (item.Contains("<span class=\"money\"><span class=money>")) {
-                        var tmp = item.Replace("<span class=\"money\"><span class=money>", "");
-                        price = tmp.Substring(0, tmp.IndexOf("</"));
-                    }
-                    if (item.Contains("<span class=\"product-swatches__label\">")) {
-                        var tmp = item.Replace("<span class=\"product-swatches__label\">", "");
-                        Types.Add(tmp.Substring(0, tmp.IndexOf("</")).Trim());
+                    if (current != null) {
+                        if (item.Contains("<img class=\"product_card__image\" src=")) {
+                            var tmp = item.Replace("<img class=\"product_card__image\" src=\"", "");
+                            current.imageSrc = tmp.Substring(0, tmp.IndexOf('"'));
+                        }
+                        if (item.Contains("<a href=\"/collections/all/") && item.Contains("class=\"title\">")) {
+                            var tmp = item.Replace("<a href=\"", "");
+                            current.href = tmp.Substring(0, tmp.IndexOf("\""));
+                            current.productTitle = item.Substring(item.IndexOf('>')).Replace("</a>", "");
+                        }
+                        if (item.Contains("<span class=\"money\"><span class=money>")) {
+                            var tmp = item.Replace("<span class=\"money\"><span class=money>", "");
+                            current.price = tmp.Substring(0, tmp.IndexOf("</"));
+                        }
+                        if (item.Contains("<span class=\"product-swatches__label\">")) {
+                            var tmp = item.Replace("<span class=\"product-swatches__label\">", "");
+                            current.Types.Add(tmp.Substring(0, tmp.IndexOf("</")).Trim());
+                        }
                     }
                 }
-                result.Add(string.Format("{0}@{1}@{2}@{3}@{4}@{5}", num, imageSrc.Trim(), productHref.Trim(), productTitle.Trim(), price.Trim(), string.Join(", ", Types).Trim()));
-                Types.Clear();
+                result.Add(current);
 
-                File.AppendAllLines("test.csv", result);
+                File.AppendAllLines("test.csv", result.Where(product=>product.IsValid()).Select(product=>product.ToString()));
             }
         }   
     }
---
+++ Kingice_Parser/Kingice_Parser/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Kingice_Parser {
+    class Product {
+        public int num = 0;
+        public string imageSrc = "";
+        public string productHref = "";
+        public string productTitle = "";
+        public string price = "";
+        public string href = "";
+
+        public List<string> Types = new List<string>();
+
+        public bool IsValid() {
+            return !string.IsNullOrEmpty(productTitle);
+        }
+
+        public override string ToString() {
+            return string.Format("{0}@{1}@{2}@{3}@{4}@{5}",
+                num, imageSrc.Trim(), productHref.Trim(), productTitle.Trim(), price.Trim(), string.Join(", ", Types).Trim());
+        }
+    }
+}
